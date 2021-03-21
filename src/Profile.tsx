988b5e290375764f62fdae465838fd1fc4ee6abd
UmkaPC <lamborginitv322@gmail.com>
2021-03-21 15:06:06 +0300

    2

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import {Card, CardContent, CardMedia, Grid, Typography} from "@material-ui/core";
 import Chart from "react-google-charts";
+import css from './Profile.module.css';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        padding: '0 30px',
+    },
+});
 
 function UserCard() {
+    const classes = useStyles();
     return (
-        <Card>
+        <Card className={classes.root}>
             <Grid container spacing={3}>
                 <CardContent>
                     <Grid item xs={6}>
@@ -67,7 +80,7 @@ function KPETableCurrentDate() {
                ]/*Enter data here*/}
                options={{
                    title: 'name',
-                   chartArea: {width: '30%'},
+                   chartArea: {width: '100%'},
                    hAxis: {
                        title: 'Name x axis',
                        minValue: 0,
@@ -96,7 +109,7 @@ function KPEDynamicTableQuarter() {
                ]/*Enter data here*/}
                options={{
                    title: 'name',
-                   chartArea: {width: '30%'},
+                   chartArea: {width: '100%'},
                    hAxis: {
                        title: 'Name x axis',
                        minValue: 0,
