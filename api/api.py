e7e4b6dcd7ffe683fd4be845826e20b66ddda068
Stepan <golangman@Stepans-MacBook-Pro.local>
2021-03-24 18:00:47 +0300

    added cors

--- api/api.py
+++ api/api.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+app.config["CORS_HEADERS"] = "Content-Type"
+CORS(app)
 
 
 @app.route("/timestamp", methods=["GET"])
