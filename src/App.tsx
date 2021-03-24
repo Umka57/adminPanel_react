9ae0cc3e78cfe3c1ded8d9bf791d3377256e86fe
UmkaPC <lamborginitv322@gmail.com>
2021-03-24 13:39:17 +0300

    changed file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from "./Header";
+import Header from "./Header/Header";
 import {BrowserRouter, Route} from "react-router-dom"
-import Profile from "./Profile";
-import Users from "./UsersPage";
+import Profile from "./Profile/Profile";
+import Users from "./UsersPage/UsersPage";
 function App() {
     return (
         <BrowserRouter>
