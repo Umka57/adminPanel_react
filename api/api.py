7a9c31cabb82ff52135226c119e029a3b7032425
golangman <golangman@aol.com>
2021-04-02 16:40:36 +0300

    fix

--- api/api.py
+++ api/api.py
@@ -1,60 +1,100 @@
 # DEVELOPED BY BOOSTBOTS
 from datetime import datetime
-from flask import Flask
+from json import dumps
+from logging import exception
+from settings import DestinationsGetInputData, DestinationsGetPossibleInputData
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from database import *
-import
+from pydantic import ValidationError
+from traceback import format_exc
+
+
+ANSWER_ERROR = dict(response=0)
+ANSWER_RIGHT = dict(response=1)
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/YOURAPP/*": {"origins": "*"}})
 
 
-@app.route("/table.get", methods=["GET"])
-def table_get():
-    user_id, quarter, year = (
-        request.args.get("user_id"),
-        request.args.get("quarter"),
-        request.args.get("year"),
-    )
-
-    if not (user_id and quarter and year):
-        return ANSWER_ERROR, 400
-
-    last_month = 3 * int(quarter)
-    year = int(year)
-    user_id = int(user_id)
+@app.route("/destinations.get", methods=["POST"])
+def destinations_get():
+    try:
+        inputData = DestinationsGetInputData.parse_raw(dumps(request.json))
+    except ValidationError as error:
+        return error.json(), 400
+    except:
+        return format_exc(), 500
 
     try:
-        tables = (
-            Destination.select()
-            .join(
-                DestinationValues,
-                on=(Destination.id == DestinationValues.destination_id),
-            )
-            .where(
-                (Destination.user == user_id)
-                & (Destination.year == year)
-                & (DestinationValues.month in list(range(last_month - 3, last_month)))
-            )
+        tables = Destination.select().where(
+            (Destination.id == inputData.destination_id)
         )
 
     except Exception as e:
-        from traceback import format_exc
 
-        print(format_exc())
-        return ANSWER_ERROR, 500
+        return format_exc(), 500
 
     try:
-        return dumps([i.__dict__["__data__"] for i in tables], ensure_ascii=False), 200
+        print([i.__dict__["__data__"] for i in tables])
+        return dumps([i.__dict__["__data__"] for i in tables]), 200
     except Exception as e:
-        return ANSWER_ERROR, 400
+        return format_exc(), 500
+
+
+@app.route("/destinations.getValues", methods=["POST"])
+def destinations_get_values():
+    try:
+        inputData = DestinationsGetInputData.parse_raw(dumps(request.json))
+    except ValidationError as error:
+        return error.json(), 400
+    except:
+        return format_exc(), 500
+
+    try:
+        tables = DestinationValues.select().where(
+            (DestinationValues.destination == inputData.destination_id)
+        )
+
+    except Exception as e:
+
+        return format_exc(), 500
+
+    try:
+        return dumps([i.__dict__["__data__"] for i in tables]), 200
+    except Exception as e:
+        return format_exc(), 500
+
+
+@app.route("/destinations.getPossible", methods=["POST"])
+def destinations_get_possible():
+    try:
+        inputData = DestinationsGetPossibleInputData.parse_raw(dumps(request.json))
+    except ValidationError as error:
+        return error.json(), 400
+    except:
+        return format_exc(), 500
+
+    try:
+        data = Destination.select().where(Destination.user == inputData.user_id)
+
+        return_data = {"count": len(data)}
+        respone = []
+
+        for destination in data:
+            respone.append(destination.__dict__["__data__"])
+
+        return_data.update(dict(response=respone))
+        from pprint import pprint
+
+        pprint(return_data)
+        return return_data, 200
+
+    except Exception as e:
+
+        return format_exc(), 500
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     database.close()
-
-
-# @app.route("/timestamp", methods=["GET"])
-# def home():
-#     pass
