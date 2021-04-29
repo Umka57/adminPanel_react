ec25ed18d4a075c4353c20a03b1af241fac84dff
UmkaPC <lamborginitv322@gmail.com>
2021-04-29 23:45:07 +0300

    without devtools

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -7,7 +7,7 @@ export const store = createStore(rootReducer,
     compose(
         applyMiddleware(
             thunk
-        ),
+        )/*,
         //@ts-ignore
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
     ))
