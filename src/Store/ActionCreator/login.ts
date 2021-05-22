86e5ff67ca6c1ad6110f04cd92956b34a8f74e2e
Umka57Notebook <lamborginitv322@gmail.com>
2021-05-22 15:00:30 +0300

    4server 3

--- src/Store/ActionCreator/login.ts
+++ src/Store/ActionCreator/login.ts
@@ -2,12 +2,21 @@ import {Dispatch} from "redux";
 import axios from "axios";
 import {LoginActions, LoginActionTypes} from "../../Types/login";
 
+const instance = axios.create({
+    withCredentials:true,
+    baseURL: 'http://185.193.143.232:3000',
+    timeout: 1000,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'}
+});
+
 export const signIn = (input:any = null) => {
     return async (dispatch: Dispatch<LoginActions>) => {
         try {
             dispatch({type: LoginActionTypes.LOGIN})
             const md5 = require('md5')
-            const response = await axios.post("/auth.sign-in",{"login":input.login,"access_token":md5(input.password)})
+            const response = await instance.post("auth.sign-in",{"login":input.login,"access_token":md5(input.password)})
             dispatch({type: LoginActionTypes.LOGIN_SUCCESS, payload: response.data.response})
         } catch (e) {
             dispatch({type: LoginActionTypes.LOGIN_ERROR, payload: 'Неверный логин или пароль'})
