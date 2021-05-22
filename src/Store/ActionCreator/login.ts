156a96a9e224c00b5b19c4cb909b25cc2e541a40
Umka57Notebook <lamborginitv322@gmail.com>
2021-05-22 14:44:44 +0300

    4server 2

--- src/Store/ActionCreator/login.ts
+++ src/Store/ActionCreator/login.ts
@@ -7,7 +7,7 @@ export const signIn = (input:any = null) => {
         try {
             dispatch({type: LoginActionTypes.LOGIN})
             const md5 = require('md5')
-            const response = await axios.post("/auth.sing-in",{"login":input.login,"access_token":md5(input.password)})
+            const response = await axios.post("/auth.sign-in",{"login":input.login,"access_token":md5(input.password)})
             dispatch({type: LoginActionTypes.LOGIN_SUCCESS, payload: response.data.response})
         } catch (e) {
             dispatch({type: LoginActionTypes.LOGIN_ERROR, payload: 'Неверный логин или пароль'})
